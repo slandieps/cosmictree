14755cfe6f006c779a83035e6c258185024b7134
slandie ps <slandieps@gmail.com>
2025-05-12 13:03:58 -0400

    Add files via upload
    first commit

---
+++ cosmic-tree/js/main.js
@@ -0,0 +1,26 @@
+// Get NASA's Picture of the Day by date
+
+//Event listener on click
+document.querySelector('button').addEventListener('click', getDate);
+
+//Data retrieval
+function getDate() {
+  let userDate = document.getElementById('customDate').value;
+  console.log(userDate);
+
+  const url = `https://api.nasa.gov/planetary/apod?api_key=EYNzzaJQRLs9LCZkxomYTK4knCetnwwDdPw5h7aL&date=${userDate}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      document.querySelector('img').src = data.hdurl;
+      document.querySelector('.event').innerText = data.title;
+      document.querySelector('h3').innerText = `Deep Dive: ${data.explanation}`;
+    })
+    .catch(error => console.error('Error fetching data:', error));
+}
+
+
+
+EYNzzaJQRLs9LCZkxomYTK4knCetnwwDdPw5h7aL 
